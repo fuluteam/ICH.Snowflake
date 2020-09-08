5bb7bea9e4ad41304d82128cda16c8c09b9eb84e
张思凯 <zsk308311170@hotmail.com>
2020-09-08 14:19:25 +0800

    1

--- test/ICH.Snowflake.Test/Program.cs
+++ test/ICH.Snowflake.Test/Program.cs
@@ -8,14 +8,14 @@ namespace ICH.Snowflake.Test
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(1L<<12);
+            Console.WriteLine(1L << 12);
 
             var services = new ServiceCollection();
-            services.AddSnowflake(opt => opt.WorkId = 19);
             services.AddSnowflakeWithRedis(opt =>
             {
                 opt.InstanceName = "aaa:";
                 opt.ConnectionString = "10.0.0.146";
+                opt.WorkIdLength = 9;
                 opt.RefreshAliveInterval = TimeSpan.FromHours(1);
             });
             var idMaker = services.BuildServiceProvider().GetService<ISnowflakeIdMaker>();
